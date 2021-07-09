ballerina-platform/module-ballerina-auth

Update directory structure and package names

ballerina/init.bal
@@ -21,5 +21,5 @@ isolated function init() {
 }
 
 isolated function setModule() = @java:Method {
-    'class: "org.ballerinalang.stdlib.auth.ModuleUtils"
+    'class: "io.ballerina.stdlib.auth.ModuleUtils"
 } external;

native/src/main/java/io/ballerina/stdlib/auth/ModuleUtils.java
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-package org.ballerinalang.stdlib.auth;
+package io.ballerina.stdlib.auth;
 
 import io.ballerina.runtime.api.Environment;
 import io.ballerina.runtime.api.Module;

native/src/main/java/module-info.java
@@ -19,5 +19,5 @@ module io.ballerina.stdlib.auth {
     requires io.ballerina.runtime;
     requires java.naming;
     requires org.slf4j;
-    exports org.ballerinalang.stdlib.auth;
+    exports io.ballerina.stdlib.auth;
 }

ballerina-platform/module-ballerina-oauth2

ballerina/init.bal
@@ -21,5 +21,5 @@ isolated function init() {
 }
 
 isolated function setModule() = @java:Method {
-    'class: "org.ballerinalang.stdlib.oauth2.ModuleUtils"
+    'class: "io.ballerina.stdlib.oauth2.ModuleUtils"
 } external;

native/src/main/java/io/ballerina/stdlib/oauth2/ModuleUtils.java
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-package org.ballerinalang.stdlib.oauth2;
+package io.ballerina.stdlib.oauth2;
 
 import io.ballerina.runtime.api.Environment;
 import io.ballerina.runtime.api.Module;

native/src/main/java/module-info.java
@@ -19,5 +19,5 @@ module io.ballerina.stdlib.oauth2 {
     requires io.ballerina.runtime;
     requires java.net.http;
     requires io.ballerina.stdlib.crypto;
-    exports org.ballerinalang.stdlib.oauth2;
+    exports io.ballerina.stdlib.oauth2;
 }